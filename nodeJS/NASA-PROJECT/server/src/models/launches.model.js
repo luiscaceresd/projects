8847f625bce7098e3e3eeab7e1dabc71f9be9ffe
Luis Caceres <luiscaceresd97@gmail.com>
2022-02-25 16:31:45 -0500

    NASA-PROJECT now supports versioning!

--- nodeJS/NASA-PROJECT/server/src/models/launches.model.js
+++ nodeJS/NASA-PROJECT/server/src/models/launches.model.js
@@ -1,8 +1,9 @@
 const launchesDatabase = require('./launches.mongo')
+const planets = require('./planets.mongo')
+
+const DEFAULT_FLIGHT_NUMBER = 100
 
 const launches = new Map()
-
-let latestFlightNumber = 100
 
 const launch = {
   flightNumber: 100,
@@ -17,8 +18,22 @@ const launch = {
 
 saveLaunch(launch)
 
-function existsLaunchWithId(launchId){
-  return launchesDatabase.has(launchId)
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({
+    flightNumber: launchId,
+  })
+}
+
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase
+    .findOne()
+    .sort('-flightNumber')
+
+  if(!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER
+  }
+
+    return latestLaunch.flightNumber
 }
 
 async function getAllLaunches() {
@@ -27,36 +42,48 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  await launchesDatabase.updateOne({
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  })
+
+  if(!planet) {
+    throw new Error('No matching planet found')
+  }
+
+  await launchesDatabase.findOneAndUpdate({
     flightNumber: launch.flightNumber,
   }, launch, {
     upsert: true,
   })
 }
 
-function addNewLaunch(launch){
-  latestFlightNumber++
-  launches.set(
-    latestFlightNumber, 
-    Object.assign(launch,{
-      succes: true,
-      upcoming: true,
-      customers: ['ZTM', 'NASA'],
-      flightNumber: latestFlightNumber,
-    })
-  ) 
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = await getLatestFlightNumber() + 1
+  
+  const newLaunch = Object.assign(launch, {
+    success: true,
+    upcoming: true,
+    customers: ['ZTM','NASA'],
+    flightNumber: newFlightNumber
+  })
+
+  await saveLaunch(newLaunch)
 }
 
-function abortLaunchById(launchId){
-  const aborted = launches.get(launchId)
-  aborted.upcoming = false
-  aborted.success = false
-  return aborted
+async function abortLaunchById(launchId){
+  const aborted = await launchesDatabase.updateOne({
+    flightNumber: launchId,
+  }, {
+    upcoming: false,
+    success: false,
+  })
+
+  return aborted.modifiedCount === 1
 }
 
 module.exports = {
   existsLaunchWithId,
   getAllLaunches,
-  addNewLaunch,
+  scheduleNewLaunch,
   abortLaunchById,
 }
--- nodeJS/NASA-PROJECT/server/src/routes/launches/launches.router.js
+++ nodeJS/NASA-PROJECT/server/src/routes/launches/launches.router.js
@@ -8,8 +8,8 @@ const {
 
 const launchesRouter = express.Router()
 
-launchesRouter.get('/', httpGetAllLaunches)
-launchesRouter.post('/', httpAddNewLaunch)
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunch);
 launchesRouter.delete('/:id', httpAbortLaunch)
 
 module.exports = launchesRouter
