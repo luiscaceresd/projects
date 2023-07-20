33c5bd1dc8da555d5e6399ab870cd604a259a704
Luis Caceres <caceres@hqrentalsoftware.com>
2023-07-19 21:36:56 -0500

    updating

--- php/demo/controllers/note.php
+++ php/demo/controllers/note.php
@@ -2,10 +2,22 @@
 
 $config = require 'config.php';
 $db = new Database($config['database']);
+
 $heading = 'Note';
+$currentUserId = 1;
 
 $query = "select * from notes where id = :id";
 
-$note = $db->query($query, ['id' => $_GET['id']])->fetch();
+$note = $db->query($query, [
+    'id' => $_GET['id']
+])->fetch();
+
+if (!$note) {
+    abort();
+}
+
+if ($note['user_id'] != $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
