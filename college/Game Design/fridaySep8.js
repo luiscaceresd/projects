4ef2eff2ec565c6a4798a01422f182568c15ed3f
cacereshq <85709758+cacereshq@users.noreply.github.com>
2023-09-09 13:17:23 -0300

    class 2

---
+++ college/Game Design/fridaySep8.js
@@ -0,0 +1,41 @@
+/* 
+
+What is a game? -> any activity engaged in for diversion or amusement entertainment doesn't necessarily have an end
+
+What is a video game? -> game played by electronically manipulating images produced by a computer program on a monitor or other display
+
+Developer's definition -> Complicated set of systems working together to create an interactive experience
+
+Systemms -> Gameplay systems, physics systems, rendering systems, etc
+
+Game designers role -> create the 'creative blueprint' that makes up the game
+
+Game Design Document -> GDD -> the what
+Technical Design Document -> TDD -> the how
+Differences -> GDD is more abstract, TDD is more technical  -> GDD is more about the game, TDD is more about the code
+
+Game Design Document
+Purpose
+    Creative Blueprint -> Outlines overall vision, concept and design of the game
+    Player Experience -> What the player will experience while playing the game
+Content
+    Concept -> Overall vision of the game
+    Gameplay -> How the game will be played
+Audience and market considerations
+    Target audience -> Who will play the game
+    Market analysis -> What is the market for the game
+High level overview
+    Scope -> How big is the game
+    Less technical -> No coding
+
+Check out doom bible
+
+Footprint -> How much space will the game take up on the hard drive
+
+Watch links at the end of the presentation
+
+Come up with an idea of a game
+
+Particle manipulation
+
+*/
