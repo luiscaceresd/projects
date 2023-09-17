aad27493800d2f68e23c5f13916736e91cec4779
luiscaceresd <luiscaceresd97@gmail.com>
2023-09-17 19:41:14 -0300

    updating

---
+++ C#/CaceresConsole/CaceresConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CaceresConsole
+{
+    // Luis Caceres
+    // Sept. 20
+    // My first console app
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Please enter your name here -> ");
+            string myName = Console.ReadLine();
+            string city = "Sogamoso";
+            string country = "Colombia";
+            string interests = "I like videogames and cats";
+            Console.WriteLine("The name entered was " +  myName);
+            Console.WriteLine("I am from " + city + ", " + country);
+            Console.WriteLine(interests);
+            Console.ReadKey();
+        }
+    }
+}
