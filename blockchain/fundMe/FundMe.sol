16a48387265835e41bac1adf06254d6b707b723f
luiscaceresd <luiscaceresd97@gmail.com>
2023-12-15 14:12:01 -0400

    adding data feed from chainlink

--- blockchain/fundMe/FundMe.sol
+++ blockchain/fundMe/FundMe.sol
@@ -1,13 +1,36 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5e18;
 
     function fund () public payable {
-        //reverts if minimum amount isn't 5 USD
-        require(msg.value >= minimumUsd, "Didn't send enough ETH");
+
+        require(getConversionRate(msg.value)  >= minimumUsd, "Didn't send enough ETH");
+        //18 decimals
+    }
+
+    function getPrice() public view returns (uint256) {
+        //address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        //price of ETH in terms of USD
+        //2000.00000000
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 
     // function withdraw () public {
