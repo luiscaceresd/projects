2e9bdd8acb6bcd6aab49e2ef62c0ff4119608f0d
cacereshq <85709758+cacereshq@users.noreply.github.com>
2023-12-19 13:37:50 -0400

    optimizing gas

--- blockchain/fundMe/FundMe.sol
+++ blockchain/fundMe/FundMe.sol
@@ -3,22 +3,26 @@ pragma solidity ^0.8.18;
 
 import { PriceConverter } from "./PriceConverter.sol";
 
+//constant, immutable -> variables can't be changed
+//771,527 gas - 751,166 after const - 727,997 after immutable -> gas saved = 20,000
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund () public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH");
         //18 decimals
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
@@ -48,7 +52,13 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        require( msg.sender ==owner, "Sender is not owner!" );
+        //require( msg.sender == i_owner, "Sender is not owner!" );
+        if(msg.sender != i_owner) { revert NotOwner();}
         _;
     }
+
+    //what happens if someone sends this contract ETH without calling fund ftn
+
+    // receive
+    // fallback
 }
